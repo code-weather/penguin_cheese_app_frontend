6585bf7c00e0154befee02a5fe7bb33ea3e74adc
Jameson Wang <jamesonw10@gmail.com>
2021-11-13 17:10:14 -0500

    updated index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -69,8 +69,8 @@ if (props.cheeses) {
                         <Link to={`/cheese/${cheese._id}`}>
                             <h1>{cheese.name}</h1>
                         </Link>
-                        <img src={cheese.image} alt={cheese.name} />
                         <h3>{cheese.countryOfOrigin}</h3>
+                        <img src={cheese.image} alt={cheese.name} />
                     </div>
             );
             })}
